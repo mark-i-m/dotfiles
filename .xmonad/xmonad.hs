791d5a762dd24fef5b5e5e7e3b420e5adeb0cc88
mark-i-m <mark-i-m@users.noreply.github.com>
2014-10-30 12:28:35 -0500

    Update xmonad.hs
    Made the config more Haskell-y
    Removed extra imports
    Started xmobar through Haskell, so it will automatically restart with xmonad
    Killed trayer and nm-applet when restarted xmonad, so there are not many running in the bg
    Renamed last space for email app

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,25 +1,29 @@
-import XMonad
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.Script
-import XMonad.Util.Run (spawnPipe)
-import XMonad.Util.EZConfig (additionalKeys)
-import System.IO
 
-main = do
-    xmproc <- spawnPipe "/usr/bin/xmobar ~/.xmobarrc"
-    xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "green" "" . shorten 120
-                        }
-        , startupHook = execScriptHook "'~/.xmonad/nautilus_trayer.sh'" -- for automount and background
-        } `additionalKeys`
-           [  ((mod4Mask, xK_l), spawn "slock")
-            , ((0                     , 0x1008FF11), spawn "amixer set Master 2-")
-            , ((0                     , 0x1008FF13), spawn "amixer set Master 2+")
-            , ((0                     , 0x1008FF12), spawn "amixer -D pulse set Master 1+ toggle")
-            , ((0                     , 0x1008FFA9), spawn "touchpad-toggle")
-           ]
+import XMonad
+import XMonad.Hooks.DynamicLog (xmobar)
+import XMonad.Hooks.Script
+import qualified Data.Map as M
+
+myWorkspaces = map show [1..8] ++ ["email"]
+
+myKeys (XConfig {modMask = modm}) = M.fromList $
+    [  ((mod4Mask, xK_l), spawn "slock")
+    , ((0    , 0x1008FF11), spawn "amixer set Master 2-")
+    , ((0    , 0x1008FF13), spawn "amixer set Master 2+")
+    , ((0    , 0x1008FF12), spawn "amixer -D pulse set Master 1+ toggle")
+    , ((0    , 0x1008FFA9), spawn "touchpad-toggle")
+    , ((modm , xK_q      ), spawn "killall trayer nm-applet ; xmonad --recompile && xmonad --restart")
+    ]
+
+myConfig = defaultConfig {
+    -- automount, desktop background, systray
+    startupHook = execScriptHook "'/home/mark/.xmonad/nautilus_trayer.sh'",
+    
+    -- workspace names
+    workspaces  = myWorkspaces,
+
+    -- keyBindings
+    keys        = \c -> myKeys c `M.union` keys defaultConfig c
+    }
+
+main = xmonad =<< xmobar myConfig
