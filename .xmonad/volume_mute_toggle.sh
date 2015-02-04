322dcdc7fee3b6b186ad019711d21dd19e73f668
mark-i-m <mark-i-m@users.noreply.github.com>
2015-02-04 13:16:55 -0600

    Create volume_mute_toggle.sh

---
+++ .xmonad/volume_mute_toggle.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+v1=`amixer get Master | grep -o -G "\[[0-9]*%\]"`
+v2=`amixer get Master | fgrep -o [on]`
+v3=`amixer get Master | fgrep -o [off]`
+
+if [ "$v2" = "[on]" ]; then
+    amixer set Master 1+ toggle
+fi
+
+if [ "$v3" = "[off]" ]; then
+    amixer set Master 1+ toggle
+    #amixer set Speaker 1+ toggle
+    #amixer set Headphone 1+ toggle
+    #amixer set PCM 1+ toggle
+fi
