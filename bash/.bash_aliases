0439fff39e4418c8a274ebde7eecf1071c5cabc3
Mark Mansi <mmansi@utexas.edu>
2015-05-30 13:53:20 -0700

    Some cleanup and add bash_aliases

---
+++ bash/.bash_aliases
@@ -0,0 +1,14 @@
+alias touchpad-disable='xinput disable "AlpsPS/2 ALPS GlidePoint"'
+alias touchpad-enable='xinput enable "AlpsPS/2 ALPS GlidePoint"'
+
+alias sl="ls"
+
+alias pyhton='python'
+alias python='python2'
+
+alias ...="cd .."
+alias ....="cd ../.."
+alias .....="cd ../../.."
+alias cd..="cd .."
+
+alias cdjnk="cd ~/Downloads/junk"
--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ INSTALL_XINITRC=False
 # bash
 INSTALL_BASHRC=False
 INSTALL_BASH_ENV_VARS=False
+INSTALL_BASH_ALIASES=False
 
 # xmonad
 INSTALL_XMONAD_CONFIG=False
@@ -41,12 +42,23 @@ INSTALL_DIR=~
 ## DO NOT EDIT ANYTHING BELOW THIS LINE!
 #################################################################################
 
+function install_config_file() {
+    cp -r $1 $INSTALL_DIR
+}
+
+function install_vim_plugin() {
+    mkdir -p $INSTALL_DIR/.vim/bundle
+    ( cd ./vim/.vim/bundle && git submodule update --init --recursive $1 )
+    cp -r ./vim/.vim/bundle/$1 $INSTALL_DIR/.vim/bundle/
+}
+
 # check to see if we should install everything
 if [ "$INSTALL_ALL" = "True" ]; then
     INSTALL_PROFILE=True
     INSTALL_XINITRC=True
     INSTALL_BASHRC=True
     INSTALL_BASH_ENV_VARS=True
+    INSTALL_BASH_ALIASES=True
     INSTALL_XMONAD_CONFIG=True
     INSTALL_XMOBARRC=True
     INSTALL_VIMRC=True
@@ -62,34 +74,38 @@ fi
 
 # install xorg stuff
 if [ "$INSTALL_PROFILE" = "True" ]; then
-    cp ./x/.profile $INSTALL_DIR/.profile
+    install_config_file "./x/.profile"
 fi
 
 if [ "$INSTALL_XINITRC" = "True" ]; then
-    cp ./x/.xinitrc $INSTALL_DIR/.xinitrc
+    install_config_file "./x/.xinitrc"
 fi
 
 # install bash stuff
 if [ "$INSTALL_BASHRC" = "True" ]; then
-    cp ./bash/.bashrc $INSTALL_DIR/.bashrc
+    install_config_file "./bash/.bashrc"
 fi
 
 if [ "$INSTALL_BASH_ENV_VARS" = "True" ]; then
-    cp ./bash/.bash_env_vars $INSTALL_DIR/.bash_env_vars
+    install_config_file "./bash/.bash_env_vars"
+fi
+
+if [ "$INSTALL_BASH_ALIASES" = "True" ]; then
+    install_config_file "./bash/.bash_aliases"
 fi
 
 # install xmonad stuff
 if [ "$INSTALL_XMONAD_CONFIG" = "True" ]; then
-    cp -r ./wm/.xmonad $INSTALL_DIR/.xmonad
+    install_config_file "./wm/.xmonad"
 fi
 
 if [ "$INSTALL_XMOBARRC" = "True" ]; then
-    cp ./wm/.xmobarrc $INSTALL_DIR/.xmobarrc
+    install_config_file "./wm/.xmobarrc"
 fi
 
 # install vim
 if [ "$INSTALL_VIMRC" = "True" ]; then
-    cp ./vim/.vimrc $INSTALL_DIR/.vimrc
+    install_config_file "./vim/.vimrc"
 fi
 
 if [ "$INSTALL_PATHOGEN" = "True" ]; then
@@ -97,12 +113,6 @@ if [ "$INSTALL_PATHOGEN" = "True" ]; then
         curl -LSso $INSTALL_DIR/.vim/autoload/pathogen.vim \
         https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
 fi
-
-function install_vim_plugin() {
-    mkdir -p $INSTALL_DIR/.vim/bundle
-    ( cd ./vim/.vim/bundle && git submodule update --init --recursive $1 )
-    cp -r ./vim/.vim/bundle/$1 $INSTALL_DIR/.vim/bundle/
-}
 
 if [ "$INSTALL_RUST_VIM" = "True" ]; then
     install_vim_plugin "rust.vim"
