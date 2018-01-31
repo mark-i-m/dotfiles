4532e82abe8f346d9a5957f95dbd2287e53fd025
Who? Me?! <mark-i-m@users.noreply.github.com>
2018-01-31 16:39:29 -0600

    Update xmonad.hs

--- wm/.xmonad/xmonad.hs
+++ wm/.xmonad/xmonad.hs
@@ -4,8 +4,9 @@ import XMonad.Actions.SpawnOn
 import XMonad.Layout.NoBorders (smartBorders)
 import XMonad.Layout.Named
 import XMonad.Layout.Groups (group)
-import XMonad.Layout.Groups.Helpers
+import XMonad.Layout.Groups.Helpers (focusUp, focusDown, swapUp, swapDown, swapMaster, moveToGroupUp, moveToGroupDown, focusGroupDown)
 --import XMonad.Layout.ThreeColumns (ThreeCol(..))
+import XMonad.Operations (refresh)
 import XMonad.Hooks.ManageDocks (avoidStruts, manageDocks)
 import XMonad.Util.Run (spawnPipe)
 import qualified XMonad.StackSet as W
@@ -21,19 +22,29 @@ myWorkspaces = map show [1..9]
 myKeys conf@(XConfig {modMask = modm}) = fromList $
     [ ((modm, xK_z), spawn "slock")
     , ((modm, xK_q), spawn "bash ~/.xmonad/restart.sh")
+    
+    -- keybindings for workspaces
     , ((modm, xK_Right), nextWS)
     , ((modm, xK_Left), prevWS)
     , ((modm .|. shiftMask, xK_Right), shiftToNext >> nextWS)
     , ((modm .|. shiftMask, xK_Left), shiftToPrev >> prevWS)
     , ((modm, xK_f), moveTo Next EmptyWS)
     , ((modm .|. shiftMask, xK_f), shiftTo Next EmptyWS)
+    -- keybindings within workspaces
     , ((modm, xK_Tab), focusDown)
     , ((modm .|. shiftMask, xK_Tab), focusUp)
+    , ((modm, xK_j), focusDown)
+    , ((modm, xK_k), focusUp)
+    , ((modm .|. shiftMask, xK_j), swapDown >> refresh)
+    , ((modm .|. shiftMask, xK_k), swapUp >> refresh)
+    , ((modm, xK_Return), swapMaster >> refresh)
+    -- keybindings for stacked workspaces
     , ((modm, xK_grave), submap . fromList $
         [ ((modm, xK_grave), focusGroupDown)
         , ((modm, xK_Down), moveToGroupDown False)
         , ((modm, xK_Up), moveToGroupUp False)
         ])
+    
     , ((0   , 0x1008FF11), spawn "amixer set Master 2-")
     , ((0   , 0x1008FF13), spawn "amixer set Master 2+")
     , ((0   , 0x1008FF12), spawn "bash ~/.xmonad/volume_mute_toggle.sh")
