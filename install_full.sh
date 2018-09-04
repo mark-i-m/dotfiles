12e4a8c9f1546fdd53f80363ec675c9720da79b7
Mark Mansi <markm@cs.wisc.edu>
2018-09-04 09:59:39 -0500

    don't install powerline fonts

--- install_full.sh
+++ install_full.sh
@@ -1,10 +1,6 @@
 # install stuff
 sudo apt-get install build-essential cmake python-dev vim-gnome exuberant-ctags xmonad xmobar slock trayer
 
-# airline fonts
-git clone https://github.com/powerline/fonts.git
-(cd ./fonts; ./install.sh)
-
 # install rust
 curl -sSf https://sh.rustup.rs | sh -s -- -y --no-modify-path
 
